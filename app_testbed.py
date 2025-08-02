ddd307c33b296330b2d52776cb2426ed8b7d3d34
acircuit-ctrlV <acircuit.is@gmail.com>
2025-08-02 15:36:41 +0700

    Update app_testbed.py

--- app_testbed.py
+++ app_testbed.py
@@ -241,29 +241,22 @@ with col4:
 st.header("ตารางก๊วน")
 
 # Define the column configuration to freeze the "Name" column
-#column_configuration = {
-#    "Name": st.column_config.TextColumn(
-#        "Name",
-#        help="The name of the player",
-#        fixed="left"
-#    )
-#}
-#
-## Pass the column configuration to st.data_editor
-#edited_df = st.data_editor(
-#    st.session_state.df,
-#    num_rows="dynamic",
-#    use_container_width=True,
-#    key="main_data_editor",
-#    column_config=column_configuration
-#)
+column_configuration = {
+    "Name": st.column_config.TextColumn(
+        "Name",
+        help="The name of the player",
+        fixed="left"
+    )
+}
+
+# Pass the column configuration to st.data_editor
 edited_df = st.data_editor(
     st.session_state.df,
     num_rows="dynamic",
     use_container_width=True,
-    key="main_data_editor"
+    key="main_data_editor",
+    column_config=column_configuration
 )
-
 
 if st.button("Calculate"):
     st.session_state.df = edited_df
